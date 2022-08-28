8e6ca657f15d530c9d4d57cd0b393f5ecd8ef185
Luciano Gutierrez <lucianogutierrezagustin@gmail.com>
2022-08-28 17:55:21 -0300

    webviewtest

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:people_information/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        
+        home: HomePage());
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+        ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PageOne()),
+                      );
+                    },
+                    child: Container(
+                      child: Text("PageOne"),
+                    )),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      child: Text("Navegadores"),
+                    )),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      child: Text("Navegadores"),
+                    )),
+              ],
+            ),
+          ],
+        ));
+  }
+}
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: "https://fast.com/es/",
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class PageThree extends StatelessWidget {
+  const PageThree({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
